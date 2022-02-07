55bb181abc04bd6e91f337b17ca8ab88d78804da
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-07 21:25:03 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -3,14 +3,18 @@ fetch("https://api.github.com/users/hammer-01/repos", {
     headers: {
         'Accept': 'application/json'
     }
-}).then(response => response.json())
-  .then(json => {
-      let pages = [];
-      for (let repo of json) {
-          if (repo.has_pages && repo.name !== window.location.hostname) pages.push(repo);
-      }
-      return pages;
-  })
-  .then();
+})
+    .then(response => response.json())
+    .then(json => {
+        let pages = [];
+        for (let repo of json) {
+            if (repo.has_pages && repo.name !== window.location.hostname) pages.push(repo);
+        }
+        return pages;
+    })
+    .then(pages => {
+        pages.forEach(v => console.log(v));
+    });
 
 
+console.log(console.log(hi));
