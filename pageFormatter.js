0948302886d36131691f47666321ff0fae5e77c9
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-07 18:13:29 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -1,13 +1,8 @@
-console.log(fetch("https://www.google.com/robots.txt", {mode:'no-cors'}));
-
-
-
-
-
-
+fetch("https://api.github.com/users/hammer-01");
 //   .then(function(response) {
 //   return response.text();
 //   })
+
 //   .then(function(data){
 //     console.log(data); //this will just be text
 //     var data_obj = JSON.parse(data);
