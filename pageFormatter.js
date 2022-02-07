3c1869a79d0abf3ba918d969aca2c296d75d9daa
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-07 20:50:23 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -7,7 +7,7 @@ let jsonData = fetch("https://api.github.com/users/hammer-01/repos", {
   .then(json => {
       let pages = [];
       for (let repo of json) {
-          if (repo.has_pages) pages.push(repo);
+          if (repo.has_pages && repo.html_url !== window.location.href) pages.push(repo);
       }
       return pages;
   });
