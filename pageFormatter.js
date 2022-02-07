cb7551121cec7d445d9716397f1f7f44d2b50698
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-07 18:24:23 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -1,9 +1,10 @@
 fetch("https://api.github.com/users/hammer-01")
   .then(function(response) {
+    console.log(response.text());
     return response.text();
   })
   .then(function(data){
-    console.log(data); //this will just be text
+//     console.log(data); //this will just be text
     var data_obj = JSON.parse(data);
     return data_obj;
   });
