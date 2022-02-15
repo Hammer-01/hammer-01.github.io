22563285cf5d554a7d235660f92335fab326422b
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-15 21:54:52 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -25,14 +25,3 @@ fetch("https://api.github.com/users/hammer-01/repos", {
                 });
         }
     });
-
-fetch("https://api.paste.ee/v1/pastes", { // '{"description":"test","sections":[{"name":"Section1","syntax":"autodetect","contents":"Testing!"}]}'
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-Auth-Token': 'aXdc0szX7bxV4d6gT8dsaw3ZEqFNEQbcQ53s4KXdX'
-    },
-    body: '{"sections":[{"name":"Section1","contents":"Testing!"}]}'
-})
-    .then(response => response.text())
-    .then(text => console.log(text))
