2373826c8596fa89d43a855ab1d4bd392ebb11dc
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-15 20:58:34 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -29,8 +29,8 @@ fetch("https://api.github.com/users/hammer-01/repos", {
 fetch("https://api.paste.ee/v1/pastes?sections='[{\"name\":\"Section1\",\"syntax\":\"autodetect\",\"contents\":\"Testing!\"}]'", { // '{"description":"test","sections":[{"name":"Section1","syntax":"autodetect","contents":"Testing!"}]}'
     method: 'POST',
     headers: {
-        'Content-Type: application/json',
-        'X-Auth-Token: aXdc0szX7bxV4d6gT8dsaw3ZEqFNEQbcQ53s4KXdX'
+        'Content-Type': 'application/json',
+        'X-Auth-Token': 'aXdc0szX7bxV4d6gT8dsaw3ZEqFNEQbcQ53s4KXdX'
     }
 })
     .then(response => response.text())
