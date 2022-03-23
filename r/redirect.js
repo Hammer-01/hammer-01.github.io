0b7311c43ee0c5fe26c33477035aee37f5a11d6a
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-23 12:00:01 +1000

    Update redirect.js

--- r/redirect.js
+++ r/redirect.js
@@ -1,8 +1,8 @@
 import redirects from "./redirects.json";
 console.log(`Referrer: ${window.location.referrer}`);
 console.log(`Redirect url: ${window.location.search}`);
-let protocol = window.location.search.match(/^[^.\/]+:/) ? "" : "http://";
+let url = window.location.search.match(/^[^.\/]+:/) === "s:" ? redirects[window.location.search.indexOf(':')] : (window.location.search.match(/^[^.\/]+:/) ? "" : "http://") + window.location.search.slice(1);
 let pageContent = document.referrer ? `A link from the page: <a href="${document.referrer}">${document.referrer}</a> wants to redirect you.<br>` : "";
-pageContent += `Click to redirect to <a href="${protocol+window.location.search.slice(1)}">${protocol+window.location.search.slice(1)}</a>.<br>`;
+pageContent += `Click to redirect to <a href="${url}">${url}</a>.<br>`;
 if (document.referrer) pageContent += `If you do not want to visit that page, you can <a href="javascript:window.history.back()">return to the previous page</a>.`;
 document.body.innerHTML = pageContent;
