da8f5b8181f9853d28252b19ac0902523f45459e
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-24 22:18:20 +1000

    Update redirect.js

--- r/redirect.js
+++ r/redirect.js
@@ -1,8 +1,11 @@
+// TODO: Add bookmarklet special case (protocol == javascript:)
 console.log(`Referrer: ${window.location.referrer}`);
 console.log(`Redirect url: ${window.location.search}`);
-let protocol = window.location.search.slice(1).match(/^(\w+:|\/\/)/g);
-let url = protocol == "_:" ? redirects[window.location.search.slice(3)] : (protocol ? "" : "//") + window.location.search.slice(1);
+let search = window.location.search.slice(1);
+let protocol = search.match(/^(\w+:|\/\/)/g);
+let dispUrl = protocol == "_:" ? search.slice(2) : search;
+let url = protocol == "_:" ? redirects[dispUrl] : (protocol ? "" : "//") + dispUrl;
 let pageContent = document.referrer ? `A link from the page: <a href="${document.referrer}">${document.referrer}</a> wants to redirect you.<br>` : "";
-pageContent += `Click to redirect to <a href="${url}">${url}</a>.<br>`;
+pageContent += `Click to redirect to <a href="${url}">${dispUrl}</a>.<br>`;
 if (document.referrer) pageContent += `If you do not want to visit that page, you can <a href="javascript:window.history.back()">return to the previous page</a>.`;
 document.body.innerHTML = pageContent;
