66af6bd15ef98e204f4b12d295768c033d67a5e7
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-25 18:03:40 +1000

    Update redirect.js

--- r/redirect.js
+++ r/redirect.js
@@ -8,14 +8,14 @@ let dispUrl = protocol == "_:" ? search.slice(2) : search;
 let url = protocol == "_:" ? redirects[dispUrl] : (protocol ? "" : "//") + dispUrl;
 let pageContent = document.referrer ? `A link from the page: <a href="${document.referrer}">${document.referrer}</a> ` : "";
 if (protocol == "javascript:") {
-    pageContent += "has directed you to this bookmarklet: <br>";
+    pageContent += document.referrer ? "has directed you to this bookmarklet: <br>" : "Below is a bookmarklet - a piece of javscript code that runs when you click it.<br>";
     pageContent += `Drag <a href="${url}">this link</a> to your bookmark bar to save it.<br><br>`;
     pageContent += "The code for the bookmarklet is: <br>";
     pageContent += `<textarea id="bookmarklet-code"></textarea>`
     document.body.innerHTML = pageContent;
-    document.getElementById('bookmarklet-code').textContent = url;
+    document.getElementById('bookmarklet-code').textContent = decodeURIComponent(url);
 } else {
-    pageContent += "wants to redirect you.<br>";
+    if (document.referrer) pageContent += "wants to redirect you.<br>";
     pageContent += `Click to redirect to <a href="${url}">${dispUrl}</a>.<br>`;
     if (document.referrer) pageContent += `If you do not want to visit that page, you can <a href="javascript:window.history.back()">return to the previous page</a>.<br>`;
     pageContent += `You will be automatically redirected in <span id="countdown">5</span> second<span id="sec">s</span>.`
