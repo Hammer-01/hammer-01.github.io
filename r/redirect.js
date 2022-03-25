9fbde100b9e6d451eb31a436e5ecfd2cc08df448
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-25 17:03:17 +1000

    Update redirect.js

--- r/redirect.js
+++ r/redirect.js
@@ -10,9 +10,10 @@ let url = protocol == "_:" ? redirects[dispUrl] : (protocol ? "" : "//") + dispU
 let pageContent = document.referrer ? `A link from the page: <a href="${document.referrer}">${document.referrer}</a> wants to redirect you.<br>` : "";
 pageContent += `Click to redirect to <a href="${url}">${dispUrl}</a>.<br>`;
 if (document.referrer) pageContent += `If you do not want to visit that page, you can <a href="javascript:window.history.back()">return to the previous page</a>.<br>`;
-pageContent += `You will be automatically redirected in <span id="countdown">5</span> seconds.`
+pageContent += `You will be automatically redirected in <span id="countdown">5</span> second<span id="sec">s</span>.`
 document.body.innerHTML = pageContent;
 setInterval((num) => {
     num.textContent = num.textContent - 1;
-    if (num.textContent === '0') window.location.href = url;
+    if (num.textContent === '1') document.getElementById('sec').textContent = '';
+    if (num.textContent === '0') document.getElementById('sec').textContent = 's'; window.location.href = url;
 }, 1000, document.getElementById('countdown'));
