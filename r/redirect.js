53424bd9ce557dce04a608835a625bd4eaeb1890
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-26 19:56:06 +1000

    Update redirect.js

--- r/redirect.js
+++ r/redirect.js
@@ -8,8 +8,12 @@ if (url.slice(0, 11) === "javascript:") {
     document.title = customRedirect ? dispUrl : "Bookmarklet";
     if (customRedirect) url = url.replaceAll('"', '%22');
     pageContent += document.referrer ? "has directed you to this bookmarklet: <br>" : "Below is a bookmarklet - a piece of javascript code that runs when you click it.<br>";
-    pageContent += `Drag <a href="${url}">${customRedirect?dispUrl:"this link"}</a> to your bookmark bar to save it.<br><br>`;
-    pageContent += "The code for the bookmarklet is: <br>";
+    pageContent += `Drag <a href="${url}">${customRedirect?dispUrl:"this link"}</a> to your bookmark bar to save it.<br>`;
+    if (customRedirect) {
+        pageContent += "Or you can drag this link instead to always have the latest version: ";
+        pageContent += `<a href="javascript:(function(){fetch('https://hammer-01.github.io/r/redirects.js').then(response=>{if (response.ok){return response.text()} ${url}; return '123456789012{%22${dispUrl}%22:%22%22}'}).then(text=>eval(eval('('+text.slice(12)+')')['${dispUrl}']))})()">${dispUrl}</a><br>`;
+    }
+    pageContent += "<br>The code for the bookmarklet is: <br>";
     pageContent += `<textarea id="bookmarklet-code" cols="60" rows="20"></textarea>`
     document.body.innerHTML = pageContent;
     document.getElementById('bookmarklet-code').textContent = decodeURIComponent(url);
