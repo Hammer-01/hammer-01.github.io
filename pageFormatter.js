49e5555b5d89b160d4abd3749aa449db5a3b8ef4
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-04-04 21:33:07 +1000

    Update pageFormatter.js

--- pageFormatter.js
+++ pageFormatter.js
@@ -1,4 +1,4 @@
-fetch("https://api.github.com/users/hammer-01/repos", {
+let repoPages = fetch("https://api.github.com/users/hammer-01/repos", {
     method: 'GET',
     headers: {
         'Accept': 'application/json'
@@ -11,9 +11,12 @@ fetch("https://api.github.com/users/hammer-01/repos", {
             if (repo.has_pages && repo.name !== window.location.hostname) pages.push(repo);
         }
         return pages;
-    })
-    .then(pages => {
-        let pageList = document.getElementById("pageList");
+    });
+
+window.onload = () => {
+    let pageList = document.getElementById("pageList");
+    let numListItems = pageList.childElementCount;
+    repoPages.then(pages => {
         for (let page of pages) {
             fetch(window.location.href + page.name)
                 .then(response => response.text())
@@ -23,8 +26,12 @@ fetch("https://api.github.com/users/hammer-01/repos", {
                     if (page.description) listValue += ` - ${page.description}`;
                     pageList.innerHTML += listValue + '</p>';
                     
-                    // Fix weird rendering bug on mobile
-                    if (pageList.childElementCount > pages.length) document.body.innerHTML = document.body.innerHTML;
+                    // Run for last list item only
+                    if (pageList.childElementCount === pages.length + numListItems) {
+                        document.getElementsByClassName('loader')[0].remove(); // Remove loader
+                        document.body.innerHTML = document.body.innerHTML; // Fix weird rendering bug on mobile
+                    }
                 });
         }
     });
+}
