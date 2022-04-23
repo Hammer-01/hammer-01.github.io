6483b6427c94f2d132bbdbe40a6ab4bab1d9c4d0
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-04-23 22:29:57 +1000

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,5 +1,5 @@
 (async function() {
-    let getUserData = keys => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? getUserData(keys) : null));
+    let getUserData = ...keys => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? getUserData(keys) : null));
     /*alert(await */fetch('https://hammer-tracking.web.app', {mode: 'no-cors'});//.then(r=>r.text())); // log to firebase cloud
     /*alert(await */fetch('https://hammer-4e70b-default-rtdb.firebaseio.com/tracking.json', { // log detailed user information to realtime database for analytics
         method: 'POST',
