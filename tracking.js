9faba678d42f8f272432af3684e31c6781bd9b02
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-04-24 12:12:28 +1000

    Fix caching of cloud logging

--- tracking.js
+++ tracking.js
@@ -1,6 +1,6 @@
 (async function() {
     let getUserData = (...keys) => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? getUserData(keys) : null));
-    fetch('https://hammer-tracking.web.app', {mode: 'no-cors'}); // log to firebase cloud
+    fetch(`https://hammer-tracking.web.app/?r=${Math.floor(Math.random()*100000)}`, {mode: 'no-cors'}); // log to firebase cloud
     fetch('https://hammer-4e70b-default-rtdb.firebaseio.com/tracking.json', { // log detailed user information to realtime database for analytics
         method: 'POST',
         body: JSON.stringify({
