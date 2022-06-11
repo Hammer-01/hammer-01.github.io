a821a48ecfc5170b02da5b6f7c7d350468e5ba5e
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-06-11 21:46:56 +1000

    Allow caching of ipregistry data for 7 days

--- tracking.js
+++ tracking.js
@@ -1,5 +1,14 @@
 (async function() {
-    let getUserData = (...keys) => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? getUserData(keys) : null));
+    let fetchUserData = keys => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? fetchUserData(keys) : null));
+    let retrieveUserData = async (...keys) => {
+        let ip = await fetch('https://api.ipify.org').then(r => r.text());
+        let userData = JSON.parse(localStorage.getItem('hammer-tracking-'+ip));
+        if (!userData || new Date(userData?.time_zone?.current_time || 0).getTime() < Date.now() - 6.048e8) { // use cache only if it exists and is less than 7 days old
+            userData = await fetchUserData(keys) || {userAgent: navigator.userAgent, userAgentData: navigator.userAgentData};
+            localStorage.setItem('hammer-tracking-'+ip, JSON.stringify(userData));
+        }
+        return userData;
+    };
     fetch(`https://hammer-tracking.web.app/?r=${Math.floor(Math.random()*100000)}`, {mode: 'no-cors'}); // log to firebase cloud
     fetch('https://hammer-4e70b-default-rtdb.firebaseio.com/tracking.json', { // log detailed user information to realtime database for analytics
         method: 'POST',
@@ -9,7 +18,7 @@
             pageTitle: document.title,
             url: window.location.href,
             referrer: document.referrer,
-            data: await getUserData('tryout', 'qrwrxvw05sbmqynn') || {userAgent: navigator.userAgent, userAgentData: navigator.userAgentData} // get user information
+            data: await retrieveUserData('tryout', 'qrwrxvw05sbmqynn') || {userAgent: navigator.userAgent, userAgentData: navigator.userAgentData} // get user information
         })
     });
 })();
