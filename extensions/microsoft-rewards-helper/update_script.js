91b7c53703705ec99eecd6b823f53f92633c91ad
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2024-01-04 19:00:27 +1000

    Update Microsoft Rewards Helper extension
    Added update info (i.e. this message) to the update notice

--- extensions/microsoft-rewards-helper/update_script.js
+++ extensions/microsoft-rewards-helper/update_script.js
@@ -8,6 +8,13 @@ if (newVersion) {
 
     let currentVersion = chrome.runtime.getManifest().version;
     if (currentVersion !== newVersion) {
+        // get the update message
+        let commitData = await fetch('https://api.github.com/repos/Hammer-01/hammer-01.github.io/commits?path=extensions/microsoft-rewards-helper/manifest.json&per_page=1')
+            .then(r => r.json());
+        let commitMessage = commitData[0].commit.message.split('\n\n')[1]; // only get the description
+        if (/[^.?!]$/.test(commitMessage)) commitMessage += '.'; // add a fullstop if there is no ending punctuation
+        let updateMessage = commitMessage ? '<br>Update info: ' + commitMessage : '';
+
         // create the notice for the update
         let updateNotice = document.createElement('div');
         updateNotice.id = 'extensionUpdateNotice';
@@ -15,21 +22,27 @@ if (newVersion) {
         <p>The Microsoft Rewards Helper extension has a new update (version ${currentVersion} -> ${newVersion}). 
         To update, <a href="https://hammer-01.github.io/extensions/#microsoft-rewards-helper" target="_blank">click here</a> 
         and follow the installation intructions. 
+        ${updateMessage}<br>
         <strong>Make sure you delete the previous extension folder before installing the new version.</strong></p>
         <p><a id="extensionHideNoticeLink" href="#">Remind me later</a><b> · </b>
         <a id="extensionNoUpdateLink" href="#">Don't show this again</a></p>`;
         
-        // add the notice to the page
-        let elm = document.querySelector('#meeGradientBanner > div');
-        elm.appendChild(updateNotice);
-        document.getElementById('extensionNoUpdateLink').onclick = () => {
-            // hide the notice and set the noUpdate flag
-            updateNotice.hidden = true;
-            chrome.storage.local.set({noUpdate: true});
-        };
-        document.getElementById('extensionHideNoticeLink').onclick = () => {
-            updateNotice.hidden = true;
-        };
+        // add the notice to the page when banner element is available
+        let id = setInterval(() => {
+            let elm = document.querySelector('#meeGradientBanner > div');
+            if (elm) {
+                clearInterval(id);
+                elm.appendChild(updateNotice);
+                document.getElementById('extensionNoUpdateLink').onclick = () => {
+                    // hide the notice and set the noUpdate flag
+                    updateNotice.hidden = true;
+                    chrome.storage.local.set({noUpdate: true});
+                };
+                document.getElementById('extensionHideNoticeLink').onclick = () => {
+                    updateNotice.hidden = true;
+                };
+            }
+        }, 50);
     }
 }
 })();
