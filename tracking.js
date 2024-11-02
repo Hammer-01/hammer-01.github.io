f3669f8934ebc62c2192fe14199e5a3cb8b1d94b
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2024-11-02 21:28:11 +1000

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,8 +1,11 @@
 (async function() {
     let fetchUserData = keys => fetch(`https://api.ipregistry.co/?key=${keys.shift()}`).then(r => r.ok ? r.json() : (keys.length ? fetchUserData(keys) : null));
-    let retrieveUserData = async (...keys) => {
-        let ip = await fetch('https://api.ipify.org').then(r => r.text()).catch(() => fetch('https://ipwho.is/?fields=ip&output=csv', {referrerPolicy: 'no-referrer'}).then(r => r.text()));
-        let userData = JSON.parse(localStorage.getItem('hammer-tracking-'+ip));
+    let retrieveUserData = async (...keys) => { //
+        let ip = await fetch('https://icanhazip.com').then(r => r.text()).catch(() => fetch('https://api.ipify.org').then(r => r.text()).catch(() => fetch('http://ip-api.com/csv/?fields=query', {referrerPolicy: 'no-referrer'}).then(r => r.text())));
+        let userData;
+        try {
+            userData = JSON.parse(localStorage.getItem('hammer-tracking-'+ip));
+        } catch {}
         let cacheTime = new Date(userData?.time_zone?.current_time || 0).getTime();
         if (!userData || cacheTime > Date.now() || cacheTime < Date.now() - 6.048e8) { // use cache only if it exists and is less than 7 days old
             userData = await fetchUserData(keys) || {userAgent: navigator.userAgent, userAgentData: navigator.userAgentData};
